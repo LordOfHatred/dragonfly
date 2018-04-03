e5ff84ee6ad08f9b25d52d01bff9cc47a2e6da52
jason <123cbfds@163.com>
2018-04-03 16:35:20 +0800

    update ws

--- ws_api/demo.php
+++ ws_api/demo.php
@@ -2,22 +2,14 @@
 
 require_once __DIR__ . '/subscribe.php';
 
-
-subscribe(function($data) {
-    echo 'huobi';
-    print_r($data);
+//  火币
+subscribeForHuobi(function($data) {
+    $ch = isset($data['ch']) ? $data['ch'] : '';
+    echo 'huobi:', $ch , "\n";
     if(isset($data['tick']) && isset($data['tick']['bids']) && isset($data['tick']['asks'])) {
-        analyze($data['tick']['bids'], $data['tick']['asks']);
+        analyzeForHuobi($data['tick']['bids'], $data['tick']['asks']);
     }
 }, 'market.btcusdt.depth.step0');
-
-//这里是我的代码，$bid是行情里面的买入价表，$ask是卖出价表
-function analyze($bids, $asks)
-{
-    var_dump($bids);
-    var_dump($asks);
-}
-
 
 /*
     Top <levels> bids and asks, pushed every second. Valid <levels> are 5, 10, or 20.
@@ -25,13 +17,26 @@ function analyze($bids, $asks)
     Stream Name: <symbol>@depth<levels>
 */
 subscribeForBinance(function($data) {
-    
     echo 'binance';
     print_r($data);
-
     if(isset($data['bids']) && isset($data['asks'])) {
-        analyze($data['bids'], $data['asks']);
+        analyzeForBinance($data['bids'], $data['asks']);
     }
 }, 'btcusdt@depth5');
 
-runAll()
+runAll();
+
+
+//这里是我的代码，$bid是行情里面的买入价表，$ask是卖出价表
+function analyzeForHuobi($bids, $asks)
+{
+    //var_dump($bids);
+    // var_dump($asks);
+}
+
+//这里是我的代码，$bid是行情里面的买入价表，$ask是卖出价表
+function analyzeForBinance($bids, $asks)
+{
+    //var_dump($bids);
+    // var_dump($asks);
+}
--- ws_api/subscribe.php
+++ ws_api/subscribe.php
@@ -6,31 +6,28 @@ use Workerman\Worker;
 use Workerman\Connection\AsyncTcpConnection;
 
 
-/*
-*订阅数据函数
-$sub_str type: string e.g market.btcusdt.depth.step0 具体请查看api
-$callback type: function 回调函数，当获得数据时会调用
-*/
-function subscribe($callback, $sub_str="market.btcusdt.depth.step0") {
-    $GLOBALS['sub_str'] = $sub_str;
-    $GLOBALS['callback'] = $callback;
-    $worker = new Worker();
-    $worker->onWorkerStart = function($worker) {
+$GLOBALS['huobi']['sub_str'] = '';
+$GLOBALS['huobi']['callback'] = '';
+
+$GLOBALS['binance']['sub_str'] = '';
+$GLOBALS['binance']['callback'] = '';
+
+$worker = new Worker();
+$worker->onWorkerStart = function($worker) {
+    // 火币网
+    if($GLOBALS['huobi']['sub_str'] && $GLOBALS['huobi']['callback']) {
         // ssl需要访问443端口
-        $con = new AsyncTcpConnection('ws://api.huobi.pro:443/ws');
-
-        // 设置以ssl加密方式访问，使之成为wss
-        $con->transport = 'ssl';
-
-        $con->onConnect = function($con) {
+        $conHuobi = new AsyncTcpConnection('ws://api.huobi.pro:443/ws');
+        $conHuobi->transport = 'ssl';
+        $conHuobi->onConnect = function($con) {
             $data = json_encode([
-                'sub' => $GLOBALS['sub_str'],
+                'sub' => $GLOBALS['huobi']['sub_str'],
                 'id' => 'depth' . time()
             ]);
             $con->send($data);
         };
 
-        $con->onMessage = function($con, $data) {
+        $conHuobi->onMessage = function($con, $data) {
             $data = gzdecode($data);
             $data = json_decode($data, true);
             if(isset($data['ping'])) {
@@ -38,14 +35,46 @@ function subscribe($callback, $sub_str="market.btcusdt.depth.step0") {
                     "pong" => $data['ping']
                 ]));
             }else{
-                call_user_func_array($GLOBALS['callback'], array($data));           
+                call_user_func_array($GLOBALS['huobi']['callback'], array($data));           
             }
         };
+        $conHuobi->connect();
+    }
 
+    // binance
+    if($GLOBALS['binance']['sub_str'] && $GLOBALS['binance']['callback']) {
+        // ssl需要访问443端口
+        $con = new AsyncTcpConnection('ws://stream.binance.com:9443/ws/' . $GLOBALS['binance']['sub_str']);
+        // 设置以ssl加密方式访问，使之成为wss
+        $con->transport = 'ssl';
+        $con->onConnect = function($con) {
+        };
+        $con->onMessage = function($con, $data) {
+            $data = json_decode($data, true);
+            if(isset($data['ping'])) {
+                $con->send(json_encode([
+                    "pong" => $data['ping']
+                ]));
+            }else{
+                call_user_func_array($GLOBALS['binance']['callback'], array($data));           
+            }
+        };
         $con->connect();
-    };
+    }
 
-    Worker::runAll();
+};
+
+
+
+
+/*
+*订阅数据函数
+$sub_str type: string e.g market.btcusdt.depth.step0 具体请查看api
+$callback type: function 回调函数，当获得数据时会调用
+*/
+function subscribeForHuobi($callback, $sub_str="market.btcusdt.depth.step0") {
+    $GLOBALS['huobi']['sub_str'] = $sub_str;
+    $GLOBALS['huobi']['callback'] = $callback;
 }
 
 
@@ -56,31 +85,8 @@ $sub_str type: string e.g market.btcusdt.depth.step0 具体请查看api
 $callback type: function 回调函数，当获得数据时会调用
 */
 function subscribeForBinance($callback, $sub_str="btcusdt@depth5") {
-    $GLOBALS['sub_str'] = $sub_str;
-    $GLOBALS['callback'] = $callback;
-    $workerForBinance = new Worker();
-    $workerForBinance->onWorkerStart = function($worker) {
-        // ssl需要访问443端口
-        $con = new AsyncTcpConnection('ws://stream.binance.com:9443/ws/' . $GLOBALS['sub_str']);
-
-        // 设置以ssl加密方式访问，使之成为wss
-        $con->transport = 'ssl';
-        $con->onConnect = function($con) {
-            
-        };
-
-        $con->onMessage = function($con, $data) {
-            $data = json_decode($data, true);
-            if(isset($data['ping'])) {
-                $con->send(json_encode([
-                    "pong" => $data['ping']
-                ]));
-            }else{
-                call_user_func_array($GLOBALS['callback'], array($data));           
-            }
-        };
-        $con->connect();
-    };
+    $GLOBALS['binance']['sub_str'] = $sub_str;
+    $GLOBALS['binance']['callback'] = $callback;
 }
 
 function runAll()
